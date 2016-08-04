e38b6bedf6e2f829ee1cf87702f43161229e2343
MazeFX <filoplast@gmail.com>
2016-08-04 07:48:29 +0200

    cleaned up helper file

--- db/helper.py
+++ db/helper.py
@@ -11,33 +11,19 @@ Date: 4-8-2016
 Python Test docstring.
 """
 
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from db.models import Base, User, Letter
 
 
 def create_new_db():
 
-    # Create an engine that stores data in the local directory's
-    # sqlalchemy_example.db file.
     engine = create_engine('sqlite:///db/db_development.db')
 
-    # Create all tables in the engine. This is equivalent to "Create Table"
-    # statements in raw SQL.
     Base.metadata.create_all(engine)
-
-    # Bind the engine to the metadata of the Base class so that the
-    # declaratives can be accessed through a DBSession instance
     Base.metadata.bind = engine
 
     DBSession = sessionmaker(bind=engine)
-    # A DBSession() instance establishes all conversations with the database
-    # and represents a "staging zone" for all the objects loaded into the
-    # database session object. Any change made against the objects in the
-    # session won't be persisted into the database until you call
-    # session.commit(). If you're not happy about the changes, you can
-    # revert all of them back to the last commit by calling
-    # session.rollback()
     session = DBSession()
 
     # Insert a Person in the person table
--- main.py
+++ main.py
@@ -19,11 +19,11 @@ import sys
 from PyQt5 import QtWidgets
 from dialogs import LoginDialog
 from MyQtness.ui_main_window import Ui_MainWindow
-#import helper
+import db.helper as db_helper
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    #helper.dbConnect()
+    db_connection = db_helper.db_connect()
 
     loginDialog = LoginDialog()
 
